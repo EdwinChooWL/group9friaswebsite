3a95f4d12b95c9e47d8af1c341bc4eef06573e5e
EdwinChooWL <edwin.choo@my.jcu.edu.au>
2015-09-23 12:46:02 +0800

    101% completed

--- FriasWebsite/js/shoppingcart.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-function totalprice() {
-	var productQuantity = document.getElementById("productquantity");
-	var productPrice = document.getElementById("productprice");
-	document.getElementById("totalprice").innerHTML = "Total Price: SGD " + (productQuantity.selectedIndex * productPrice.innerHTML).toFixed(2);
-}	
